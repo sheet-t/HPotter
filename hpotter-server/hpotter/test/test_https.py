28452dbccb5d87ea91807da82255ea553db5462d
mattmcmahon <matt.mcmahon424@gmail.com>
2019-10-27 12:22:29 -0600

    adding TLS cert for testing

--- hpotter-server/hpotter/test/test_https.py
+++ hpotter-server/hpotter/test/test_https.py
@@ -5,7 +5,6 @@ data = b'test'
 url = 'http://127.0.0.1'
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.load_verify_locations('cert.pem')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
