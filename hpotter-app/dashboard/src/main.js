eb681153e629e7a6d06bfe7297548214a5fec8ea
RobertNelsonMSUDenver <43615469+RobertNelsonMSUDenver@users.noreply.github.com>
2019-10-29 12:06:12 -0600

    Added GeoIP widget via Leaflet (#50)

--- hpotter-app/dashboard/src/main.js
+++ hpotter-app/dashboard/src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import { Icon }  from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
 
 Vue.config.productionTip = false
+
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 new Vue({
   vuetify,
