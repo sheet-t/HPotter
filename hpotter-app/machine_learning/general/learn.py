c0928b0c15af5894489b7726d0b932823d827809
Jeff-Rowell <jeffrowell@acm.org>
2019-11-02 21:59:21 -0600

    Changed some parameters

--- hpotter-app/machine_learning/general/learn.py
+++ hpotter-app/machine_learning/general/learn.py
@@ -16,7 +16,7 @@ print(checkpoints_path)
 create_checkpoints_dir(checkpoints_path)
 d = Data(path=data_path + '/benign_requests.txt')
 rnn = Model(num_layers=2, hidden_size=64, vocab=Vocabulary(), embedding_size=64)
-trainer = Trainer(batch_size=128, checkpoints_path=checkpoints_path, dropout=0.7)
+trainer = Trainer(batch_size=128, checkpoints_path=checkpoints_path, dropout=0.6)
 steps = 10 ** 6
 epochs = 1000
 train_gen = d.train_gen(batch_size=128, num_epochs=epochs)
