ba504ca859ad5f919c3a7eff57d04b590cacfef1
Khamunetri <kclark85@msudenver.edu>
2019-11-23 10:12:33 -0700

    adding docstring

--- hpotter-server/hpotter/__main__.py
+++ hpotter-server/hpotter/__main__.py
@@ -5,19 +5,17 @@ from hpotter.plugins.handler import start_plugins, stop_plugins
 from hpotter.env import logger, stop_shell, close_db
 
 def shutdown_servers(signum, frame):
+    """Stop all running plugins, shutdown docker shell, close SQLite database"""
     stop_plugins()
-    # shell might have been started by telnet, ssh, ...
     stop_shell()
     close_db()
 
-# provide a method that win32api can parse properly
 def shutdown_win_servers(signum):
     """Provide a method for shutting down servers that can be parsed by win32api"""
     stop_plugins()
     stop_shell()
     close_db()
 
-# provide signal handling for both UNIX and Windows terminals
 if sys.platform != 'win32':
     """Provide signal handling for UNIX systems"""
     if "__main__" == __name__:
