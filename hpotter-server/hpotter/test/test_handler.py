89f7a666a1333b7f802efdd0e7b2e1861a0336c5
Khamunetri <kclark85@msudenver.edu>
2019-11-28 14:10:15 -0700

    tests passing for starting/stopping plugins/networks. handler may need refactoring

--- hpotter-server/hpotter/test/test_handler.py
+++ hpotter-server/hpotter/test/test_handler.py
@@ -12,44 +12,32 @@ lock = threading.Lock()
 
 class TestHandler(unittest.TestCase):
     def setUp(self):
-        subprocess.call('docker stop $(docker ps -aq)', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        subprocess.call('docker rm $(docker ps -a -q)', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.call('docker ps -a | xargs -n 1 docker stop', stdin=None, stdout=None, stderr=None, shell=True)
+        subprocess.call('docker ps -a | xargs -n 1 docker rm', stdin=None, stdout=None, stderr=None, shell=True)
 
     def tearDown(self):
-    #    subprocess.check_call('docker stop $(docker ps -aq)', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        #stop_plugins()
         stop_shell()
         close_db()
         stop_server()
-        subprocess.check_call('docker system prune -f', shell=True)
+        subprocess.call('docker ps -a | xargs -n 1 docker stop', stdin=None, stdout=None, stderr=None, shell=True)
+        subprocess.call('docker ps -a | xargs -n 1 docker rm', stdin=None, stdout=None, stderr=None, shell=True)
     
-    def test_start_network(self):
+    def test_start_and_stop_network(self):
         n = len(client.networks.list())
-        start_network("test")
+        start_network("test","10.3.3.0")
         n += 1
         self.assertEqual(len(client.networks.list()), n)
         stop_network()
-        
-    def test_stop_network(self):
-        start_network("test1")
-        n = len(client.networks.list())
-        stop_network()
         n -= 1
         self.assertEqual(len(client.networks.list()), n)
-   
-    def test_start_plugins(self):
+        
+    def test_start_and_stop_plugins(self):
         start_plugins()
         test = client.containers.list()
         self.assertTrue(test)
-        #stop_plugins()
-
-    def test_stop_plugins(self):
-        #start_plugins()
         stop_plugins()
         test = client.containers.list()
         self.assertFalse(test)
-        #subprocess.call('docker stop $(docker ps -aq)', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        #subprocess.call('docker rm $(docker ps -a -q)', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     def test_read_in_config(self):
         test = read_in_config()
