5434b83b3bb15abfb0d1587b4be478cebc8e94ae
Khamunetri <kclark85@msudenver.edu>
2019-11-28 16:27:09 -0700

    adding tests for tls in handler.py

--- hpotter-server/hpotter/test/test_handler.py
+++ hpotter-server/hpotter/test/test_handler.py
@@ -1,20 +1,20 @@
 import unittest
 import docker
+import sys
+import os
 import platform
 import subprocess
+import tempfile
 
+from unittest.mock import MagicMock
 from hpotter.plugins.handler import *
-from hpotter.plugins.telnet import *
+from hpotter.plugins.telnet import stop_server
 from hpotter.env import logger, stop_shell, close_db
 
 client = docker.from_env()
 lock = threading.Lock()
 
 class TestHandler(unittest.TestCase):
-    def setUp(self):
-        subprocess.call('docker ps -a | xargs -n 1 docker stop', stdin=None, stdout=None, stderr=None, shell=True)
-        subprocess.call('docker ps -a | xargs -n 1 docker rm', stdin=None, stdout=None, stderr=None, shell=True)
-
     def tearDown(self):
         stop_shell()
         close_db()
@@ -25,10 +25,8 @@ class TestHandler(unittest.TestCase):
     def test_start_and_stop_network(self):
         n = len(client.networks.list())
         start_network("test","10.3.3.0")
-        n += 1
-        self.assertEqual(len(client.networks.list()), n)
+        self.assertGreater(len(client.networks.list()), n)
         stop_network()
-        n -= 1
         self.assertEqual(len(client.networks.list()), n)
         
     def test_start_and_stop_plugins(self):
@@ -38,6 +36,16 @@ class TestHandler(unittest.TestCase):
         stop_plugins()
         test = client.containers.list()
         self.assertFalse(test)
+    
+    def test_check_platform(self):
+        if sys.platform == 'win32':
+            self.assertEqual(check_platform(), "temp\\cert.pem")
+        else:
+            self.assertEqual(check_platform(), "/tmp/cert.pem")
+
+    def test_create_tls_cert_and_key(self):
+        create_tls_cert_and_key(MagicMock())
+        self.assertTrue(os.path.exists("hpotter/test/cert.pem"))
 
     def test_read_in_config(self):
         test = read_in_config()
