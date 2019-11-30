c0b576adaa8adf2299ef11991e8b4c4ea24135b6
David <David@Naydas-MacBook-Air-2.local>
2019-11-30 01:20:53 -0700

    created basic tests for all methods of handler.py (need to flesh out test for start_services)

--- hpotter-server/hpotter/test/test_handler.py
+++ hpotter-server/hpotter/test/test_handler.py
@@ -6,7 +6,7 @@ import platform
 import subprocess
 import tempfile
 
-from unittest.mock import MagicMock
+from unittest.mock import Mock, MagicMock
 from hpotter.plugins.handler import *
 from hpotter.plugins.telnet import stop_server
 from hpotter.env import logger, stop_shell, close_db
@@ -19,16 +19,17 @@ class TestHandler(unittest.TestCase):
         stop_shell()
         close_db()
         stop_server()
-        subprocess.call('docker ps -a | xargs -n 1 docker stop', stdin=None, stdout=None, stderr=None, shell=True)
-        subprocess.call('docker ps -a | xargs -n 1 docker rm', stdin=None, stdout=None, stderr=None, shell=True)
-    
+        #os.remove("/tmp/cert.pem")
+        #subprocess.call('docker ps -a | xargs -n 1 docker stop', stdin=None, stdout=None, stderr=None, shell=True)
+        #subprocess.call('docker ps -a | xargs -n 1 docker rm', stdin=None, stdout=None, stderr=None, shell=True)
+
     def test_start_and_stop_network(self):
         n = len(client.networks.list())
         start_network("test","10.3.3.0")
         self.assertGreater(len(client.networks.list()), n)
         stop_network()
         self.assertEqual(len(client.networks.list()), n)
-        
+
     def test_start_and_stop_plugins(self):
         start_plugins()
         test = client.containers.list()
@@ -36,7 +37,7 @@ class TestHandler(unittest.TestCase):
         stop_plugins()
         test = client.containers.list()
         self.assertFalse(test)
-    
+
     def test_check_platform(self):
         if sys.platform == 'win32':
             self.assertEqual(check_platform(), "temp\\cert.pem")
@@ -44,8 +45,23 @@ class TestHandler(unittest.TestCase):
             self.assertEqual(check_platform(), "/tmp/cert.pem")
 
     def test_create_tls_cert_and_key(self):
-        create_tls_cert_and_key(MagicMock())
-        self.assertTrue(os.path.exists("hpotter/test/cert.pem"))
+        path = "hpotter/test/cert.pem"
+        create_tls_cert_and_key(path)
+        self.assertTrue(os.path.exists(path))
+        os.remove(path)
+
+    def test_check_certs(self):
+        mock = Mock()
+        check_certs(mock)
+        self.assertTrue(os.path.exists("/tmp/cert.pem"))
+
+    def test_remove_certs(self):
+        remove_certs()
+        self.assertFalse(os.path.exists("/tmp/cert.pem"))
+
+    def test_start_services(self):
+        #need to come up with a way to test this
+        start_services(MagicMock())
 
     def test_read_in_config(self):
         test = read_in_config()
